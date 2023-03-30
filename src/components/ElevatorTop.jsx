8d46e7ca22450d665dc2af1d2adaf2e02e1d8f07
Serrin Doscher <serrin.doscher53@myhunter.cuny.edu>
2023-03-30 18:26:58 -0400

    added elevator dial, numbers do not line up yet

--- src/components/ElevatorTop.jsx
+++ src/components/ElevatorTop.jsx
@@ -1,7 +1,18 @@
 const ElevatorTop = () => {
   return (
     <div className="elevator-top">
-        ElevatorTop
+        <div className="dial-container">
+          <img src="src\images\DialBlack.png"/>
+          <div className="floor-number" id="floor-one">
+            1
+          </div>
+          <div className="floor-number" id="floor-two">
+            2
+          </div>
+          <div className="floor-number" id="floor-three">
+            3
+          </div>
+        </div>
     </div>
   )
 }
