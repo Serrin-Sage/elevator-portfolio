cf0138e5af0540cb640194daccaf77a946bd0bdd
Serrin Doscher <serrin.doscher53@myhunter.cuny.edu>
2023-05-11 14:47:20 -0400

    trying out a dark version of the app, changed colors to darker tones, added gold trim to divs

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { buttonPress } from "./features/buttonPressed"
 import './App.css'
 import ElevatorTop from './components/ElevatorTop'
 import ElevatorMiddle from './components/ElevatorMiddle'
 
 function App() {
+  const dispatch = useDispatch()
 
+  useEffect(() => {
+    const delayContent = setTimeout(() => {
+      dispatch(buttonPress({ pressed: true}))
+    }, 1400)
+    return () => {
+      clearTimeout(delayContent)
+    }
+  },[])
   return (
     <div className="App">
       <ElevatorTop />
