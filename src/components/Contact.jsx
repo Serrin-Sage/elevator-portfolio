968966dd9d8bd5b9ca87f02ba390afb366d41377
Serrin Doscher <serrin.doscher53@myhunter.cuny.edu>
2023-05-13 15:44:46 -0400

    added icons to Contact page, added css to make Contact info look like a plaque

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,18 +1,35 @@
+import { Link } from "react-router-dom"
+
 const Contact = () => {
+
+  const MaillButton = ({ mailto, label}) => {
+    return (
+      <Link to="#"
+        onClick={(e) => {
+          window.open(mailto);
+          e.preventDefault();
+        }} target="_blank">
+        {label}
+      </Link>
+    )
+  }
+
   return (
     <div className="contact-container">
-      <div className="contact-content">
-        <div className="phone-container">
-          Icon of phone: 347 - 907 - 3607
-        </div>
-        <div className="email-container">
-          Email Icon: LINK serrinsage@gmail.com
-        </div>
-        <div>
-          LinkedIn Icon: LINK LinkedIn Profile
-        </div>
-        <div>
-          Github Icon: LINK Github Profile
+      <div className="contact-frame">
+        <div className="contact-content">
+          <div id="phone-container">
+            <img src="src\images\mobile-phone.png" alt="Phone Icon"/> 347 - 907 - 3607
+          </div>
+          <div className="email-container">
+            <img src="src\images\email.png" alt="Email Icon"/> <MaillButton label="Email" mailto="mailto:serrinsage@gmail.com"/>
+          </div>
+          <div>
+            <img src="src\images\In-White-128@2x.png" alt="LinkedIn Logo"/> <a href="https://www.linkedin.com/in/serrin-doscher-2a0544224/" target="_blank">LinkedIn Profile</a>
+          </div>
+          <div>
+            <img src="src\images\github-mark-white.png" alt="Github Logo"/> <a href="https://github.com/Serrin-Sage" target="_blank">Github Profile</a>
+          </div>
         </div>
       </div>
     </div>
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
+import { BrowserRouter } from "react-router-dom";
 import elevatorFloorSlice from './features/changeFloor'
 import buttonPressSlice from './features/buttonPressed'
 import './index.css'
@@ -19,9 +20,9 @@ const store = configureStore({
 })
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <BrowserRouter>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>,
+  </BrowserRouter>,
 )
