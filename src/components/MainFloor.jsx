f6aa553f0e120cf4d281b129114ed1494acf089a
Serrin Doscher <serrin.doscher53@myhunter.cuny.edu>
2023-05-14 00:06:38 -0400

    added frame to main content, looks like silver plaque now. Added style to Resume button

--- src/components/MainFloor.jsx
+++ src/components/MainFloor.jsx
@@ -5,25 +5,27 @@ function MainFloor() {
 
   return (
     <div className='main-container'>
-      <div className='main-content'>
-        <h2>Serrin Doscher</h2>
-        <div className='sub-content'>
-          <p>Software Engineer</p>
-          <p>Full-Stack Developer</p>
-        </div>
-        <div className='skills-column'>
-          {
-            skills.map((skill) => {
-              return (
-                <SkillCard skill={skill} />
-              )
-            })
-          }
-        </div>
-        <div className='resume-container'>
-          <a href={ResumePDF}  download="Serrin Doscher Resume">
-            <button> Download Resume </button>
-          </a>
+      <div className='main-frame'>
+        <div className='main-content'>
+          <h2>Serrin Doscher</h2>
+          <div className='sub-content'>
+            <p>Software Engineer</p>
+            <p>Full-Stack Developer</p>
+          </div>
+          <div className='skills-column'>
+            {
+              skills.map((skill) => {
+                return (
+                  <SkillCard skill={skill} />
+                )
+              })
+            }
+          </div>
+          <div className='resume-container'>
+            <a href={ResumePDF}  download="Serrin Doscher Resume">
+              <div> <img src="src\images\icons\DownloadIconW.png" alt="Download Icon"/> Resume </div>
+            </a>
+          </div>
         </div>
       </div>
     </div>
