85a17fe0d81eab9dd94141d1341cdfd6d0330d83
Serrin Doscher <serrin.doscher53@myhunter.cuny.edu>
2023-05-18 13:09:48 -0400

    wrote some about me blurb, not styled.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,14 +4,28 @@ const About = () => {
       <div className="about-content">
         <div className="about-image-container">
           <img src="src\images\HomerAndMe.jpg" className="about-image"/>
+          <div className="about-text-container">
+            <p>
+              Welcome to my website! My name is Serrin Doscher and I'm a software engineer and full-stack developer.
+              I have a passion for creative and unique UI designs that are fun to interactive with.
+              As a child, I used to create entire worlds through drawing, Lego building, acting, and even stop-motion animation, allowing my imagination to flourish. 
+              Today, I channel my creative energy into coding and game tinkering.
+              <br/>
+              <br/>
+              In addition to my technical experience gained over the years, I have also developed valuable team-building and cooperation skills. 
+              During my time at Flatiron School, I frequently collaborated with and supported classmates who struggled with coding concepts. 
+              These interactions fostered strong personal connections, establishing me as a dependable resource for assistance.
+            </p>
+          </div>
         </div>
-        <div className="about-text-container">
+        <div className="about-site-text">
+          <h5>About this site</h5>
           <p>
-            I'm a software engineer and full-stack web developer who's passionate about UI design and building applications that are appealing to users. As a child I would desgin whole worlds for my imagination to florish either through drawing, Lego building,
-            acting, and even some stop-motion animation. Today I apply my creative energy towards coding and game tinkering. 
-            <br/>
-            If I find a game I really enjoy, I think of ways I could modify and change some asepcts of it; like adding new rules or mechanics.
-            It's always a fun puzzle to work out, trying to get the new rules I design to work with the preexisting rules of the game
+            This site was built using React.js, Redux, and SCSS. I managed my work and project flow with Github and I'm hosting on ____.
+            The site was inspired by a talk I attended during my time at Flatiron School. The speaker made an anology about the complications
+            of designing an elevator and how the user interacts with it. This inspired me to make a wesbite that functions like a elevator,
+            although significantly less complex. The elevator dial was designed using Inkscape, everything else was desgined using vanilla
+            SCSS.
           </p>
         </div>
       </div>
