ae2df7d6d258b800b69b60445f1d51f6f4f01980
Serrin Doscher <serrin.doscher53@myhunter.cuny.edu>
2023-05-21 15:05:11 -0400

    added new nav buttons for smaller screen sizes

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,9 +1,23 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import projects from '../features/projects.json'
 import ProjectCard from "./ProjectCard"
 
 const Carousel = () => {
   const [activeIndex, setActiveIndex] = useState(0)
+  const [isScreenSmall, setIsScreenSmall] = useState(false)
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsScreenSmall(window.innerHeight < 900);
+    }
+    
+    window.addEventListener('resize', handleResize)
+    handleResize()
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    }
+  },[])
 
   const updateIndex = (newIndex) => {
     if (newIndex < 0) {
@@ -15,14 +29,36 @@ const Carousel = () => {
     setActiveIndex(newIndex)
   }
 
-  return (
-    <div className="carousel">
-        <div className="inner-items" style={{ transform: `translate(-${activeIndex * 100}%)`}}>
-            {projects.map((project) => {
-                return <ProjectCard key={project.id} project={project} width={"100%"}/>
-            })}
-        </div>
+  const SmallNavButtons = () => {
+    return (
+      <>
+       <div className="nav-buttons">
+        <>
+          <div onClick={() => updateIndex(activeIndex - 1)} className={activeIndex === 0 && "hidden-button"}>
+            <img src={"/images/icons/ProjectArrowW.png"} className="previous-button"/>
+          </div>
+          <div onClick={() => updateIndex(activeIndex + 1)} className={activeIndex === projects.length - 1 && "hidden-button"}>
+              <img src={"/images/icons/ProjectArrowW.png"} className="next-button"/>
+          </div>
+        </>
+          {/* {activeIndex !== 0 && 
+            <div onClick={() => updateIndex(activeIndex - 1)}>
+              <img src={"/images/icons/ProjectArrowW.png"}/>
+            </div>
+          }
+          {activeIndex !== projects.length - 1 && 
+            <div onClick={() => updateIndex(activeIndex + 1)}>
+              <img src={"/images/icons/ProjectArrowW.png"} className="next-button"/>
+            </div>
+          } */}
+       </div>
+      </>
+    )
+  }
 
+  const CarouselButtons = () => {
+    return  (
+      <>
         <div className="carousel-buttons">
           {activeIndex !== 0 && 
             <div>
@@ -37,6 +73,17 @@ const Carousel = () => {
             </div>
           }
         </div>
+      </>
+    )
+  }
+  return (
+    <div className="carousel">
+        <div className="inner-items" style={{ transform: `translate(-${activeIndex * 100}%)`}}>
+            {projects.map((project) => {
+                return <ProjectCard key={project.id} project={project} width={"100%"}/>
+            })}
+        </div>   
+        {isScreenSmall ? <SmallNavButtons /> : <CarouselButtons />}
     </div>
   )
 }
