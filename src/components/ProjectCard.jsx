26df300908600eda32d03a4af808afa7d3bf0e35
Serrin Doscher <serrin.doscher53@myhunter.cuny.edu>
2023-05-26 14:24:54 -0400

    corrected breakpoint from 360 to 320

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -6,14 +6,16 @@ const ProjectCard = ({ project, width }) =>  {
         <div className="project-cotent">
             <ImageSlider slides={project.images}/>
             <div className="project-text">
-                <div className="project-title">{project.title}</div>
-                <p className="project-description">{project.description}</p>
-                <div>Techn Used: {project.tech}</div>
+                {/* <div className="project-title">{project.title}</div>
+                <div className="project-description">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+                <div>Tech Used: {project.tech}</div>
                 <div className="project-links">
                     {project.demo_url === "" ? "" : `Demo: ${project.demo_url}`}
                     <img src="/images/icons/GitHubicon.png" alt="GitHub Icon"/> 
                     <a href={project.frontend_url} target="_blank">Github</a>
-                </div>
+                </div> */}
             </div>
         </div>
     </div>
